beee435caca53950fe2fc6b949efd2deb97d9d35
GIBRENNE <pierre.gibrenne@capgemini.com>
2023-04-07 11:19:42 +0200

    bonjour

---
+++ pages/car/[name]-[id].vue
@@ -0,0 +1,25 @@
+<script setup>
+const route = useRoute();
+useHead({
+  title: toTitleCase(route.params.name)
+});
+definePageMeta({
+  layout:"custom"
+})
+function toTitleCase(str){
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+</script>
+<template>
+  <div>
+    
+     
+        <CarDetailHero />  
+        <CarDetailAttributes />
+        <CarDetailDescription />
+        <CarDetailContact />
+    
+  </div>
+</template>
---
+++ pages/city/[city]/car.vue
@@ -0,0 +1,29 @@
+<script setup>
+const route = useRoute()
+useHead({
+    title:`${
+        route.params.make ? toTitleCase(route.params.make) : 'Cars'
+        } in ${toTitleCase(route.params.city)}`,
+});
+
+definePageMeta({
+  layout:"custom"
+})
+
+function toTitleCase(str){
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+</script>
+<template>
+  <div>
+   
+      <div class="mt-32 flex">
+        <CarSideBar />
+        <NuxtPage />
+      
+    </div>
+  </div>
+</template>
